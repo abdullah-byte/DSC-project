a77c6b14de396bcd55dae30af404054414cc32dd
abdullah-byte <abdullahmumuni70@gmail.com>
2020-12-10 00:05:08 +0100

    changed all files

--- project.js
+++ project.js
@@ -1,24 +1,60 @@
-let resultTitle = document.getElementById('resultTitle');
-let resultBodyA= document.getElementById('resultBodyA');
-let resultBodyB = document.getElementById('resultBodyB');
-
-async function apiCall(){
-    let queryTerm = document.getElementById('search').value;
-    queryTerm = queryTerm.split(" ").join("_");
-    if (queryTerm === ''){
-        alert('Please input something to search');
-        return false;
+async function handleSubmit(event) {
+    event.preventDefault();
+    const inputValue = document.querySelector('.js-search-input').value;
+    const searchQuery = inputValue.trim();
+  
+    const searchResults = document.querySelector('.js-search-results');
+    searchResults.innerHTML = '';
+  
+    const spinner = document.querySelector('.js-spinner');
+    spinner.classList.remove('hidden');
+  
+    try {
+      const results = await searchWikipedia(searchQuery);
+      if (results.query.searchinfo.totalhits === 0) {
+        alert('No results found. Try different keywords');
+        return;
+      }
+  
+      displayResults(results);
+    } catch (err) {
+      console.log(err);
+      alert('Failed to search wikipedia');
+    } finally {
+      spinner.classList.add('hidden');
     }
-    let fetchUrl = await fetch('https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=30&srsearch=%27' + queryTerm);
-    let data = await fetchUrl.json();
-    console.log(data);
-
-
-    resultTitle.innerText =  data.query.search[A].title;
-    resultBodyA.innerHTML = "A. " + data.query.search[A].snippet;
-    resultBodyB.innerHTML = "B. " + data.query.search[B].snippet;
-    link.href = "https://en.wikipedia.org/wiki/" + queryTerm;
-    link.innerText = "Read More";
-
-    document.querySelector('.resultContent').style.backgroundColor = "White";
-}
+  }
+  
+  async function searchWikipedia(searchQuery) {
+    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    const json = await response.json();
+    return json;
+  }
+  
+  function displayResults(results) {
+    const searchResults = document.querySelector('.js-search-results');
+  
+    results.query.search.forEach(result => {
+      const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+  
+      searchResults.insertAdjacentHTML(
+        'beforeend',
+        `<div class="result-item">
+          <h3 class="result-title">
+            <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+          </h3>
+          <a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>
+          <span class="result-snippet">${result.snippet}</span><br>
+        </div>`
+      );
+    });
+  }
+  
+  
+  const form = document.querySelector('.js-search-form');
+  form.addEventListener('submit', handleSubmit);
+  
